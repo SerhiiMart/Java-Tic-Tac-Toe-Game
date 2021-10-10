a13d3487ca7fde3d1686a0353639f54e68ca9508
Serhii <malgothsm@gmail.com>
2021-10-10 20:01:47 +0300

    Project undate : made a board

--- src/ticTacToeGame/TicTacToeGame.java
+++ src/ticTacToeGame/TicTacToeGame.java
@@ -5,15 +5,18 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		char [][] gameBoard = {
 				{' ','|',' ','|',' '},
-				{'-','+','-','+','-'},
+				{'—','+','—','+','—'},
 				{' ','|',' ','|',' '}, 
-				{'-','+','-','+','-'}, 
+				{'—','+','—','+','—'}, 
 				{' ','|',' ','|',' '}};
+		 makeGameBoard(gameBoard);	
+	}
+	public static void makeGameBoard(char [][] gameBoard) {
 		for(char[] row: gameBoard ) {
 			for(char c : row) {
 				System.out.print(c);
 			}
+			System.out.println();
 		}
 	}
-
 }
